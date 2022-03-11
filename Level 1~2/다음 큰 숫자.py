8d5763a0608df2f54ec9acc258e13bf69a2362f3
aeong98 <51521314+aeong98@users.noreply.github.com>
2022-03-11 22:28:25 +0900

    Create 다음 큰 숫자.py

---
+++ Level 1~2/다음 큰 숫자.py
@@ -0,0 +1,11 @@
+def solution(n):
+    answer = 0
+    
+    cnt = bin(n).count("1")
+    
+    for num in range(n+1, 1000001):
+        num_cnt = bin(num).count("1")
+        
+        if(cnt == num_cnt):
+            return num
+    return answer
