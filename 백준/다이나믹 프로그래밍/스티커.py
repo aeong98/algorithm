a9f4ab505bdfab6a76c3b11087e34f87a22e6f09
Seoyeon Kim <seoyeon@gentleenergycorp.com>
2022-03-17 00:45:05 +0900

    스티커

---
+++ 백준/다이나믹 프로그래밍/스티커.py
@@ -0,0 +1,33 @@
+# 0 XX
+# 1 OX
+# 2 XO
+# d[n][s] = 2*n 스티커 n번열의 상태가 s라는 것의 점수
+# a[i][j] = i행 j열의 스티거커 점수
+# d[n][0] = max(d[n-1][0], d[n-1][1], d[n-1][2]) 
+# d[n][1] = max(d[n-1][0], d[n-1][2]) + a[n][1]
+# d[n][2] = max(d[n-1][0], d[n-1][1]) +a[n][2]
+# 정답 = max(d[n][0], d[n][1], d[n][2])
+
+import sys
+
+T=int(input())
+
+def solution(n):
+    d=[[0 for _ in range(3)] for _ in range(n+1)]
+    a=[list(map(int, sys.stdin.readline().split())) for _ in range(2)]
+
+
+    d[1][1]=a[0][0]
+    d[1][2]=a[1][0]
+
+    for i in range(2,n+1):
+        d[i][0]=max(d[i-1][0], d[i-1][1], d[i-1][2])
+        d[i][1]=max(d[i-1][0], d[i-1][2]) + a[0][i-1]
+        d[i][2]=max(d[i-1][0], d[i-1][1]) + a[1][i-1]
+       
+    print(max(d[n][0], d[n][1], d[n][2]))
+    
+for i in range(T):
+    N=int(input())
+    solution(N)
+
--- 백준/다이나믹 프로그래밍/오르막 수.py
+++ 백준/다이나믹 프로그래밍/오르막 수.py
@@ -4,6 +4,7 @@
 n=int(input())
 d=[[0 for _ in range(10)] for _ in range(n+1)]
 
+# 0으로 시작하는 오르막 수 초기화
 for i in range(10):
     d[0][i]=1
 
@@ -11,7 +12,8 @@ for i in range(1,n+1):
     for j in range(10):
         for k in range(j+1):
             d[i][j]+=d[i-1][k]
- 
+
+# 끝의 수가 9인 경우를 구하면 문제의 답 도출
 print(d[n][9] % 10007)
 
 
